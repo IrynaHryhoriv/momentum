7376c3e5376671b44d78ed2715d7d95991bd37a8
Your NameIryna <irina.hr@gmail.com>
2023-02-21 23:29:45 -0800

    fix: add code for date

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function displayTime(){
 
     if (hour < 10)
     hour = "0"+ hour;
-    if (hour < 10)
+    if (min < 10)
     min = "0" + min;
     if (sec < 10)
     sec = "0" + sec;
@@ -17,8 +17,31 @@ function displayTime(){
 setInterval(displayTime,1000);
 
 
+function displayDate(){
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const monthOftheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var d = new Date();
+    let day = weekday[d.getDay()];
+    var month = monthOftheYear[d.getMonth()];
+    var numDayOfMonth = d.getDate();
+
+    document.getElementById("date").innerHTML = day + ", " + month + " " + numDayOfMonth;
+}
+setInterval(displayDate,1000);
 
 
+
+
+
+
+
+
+// function showDate(lang) {
+//     const date = new Date();
+//     const options = {month: 'long', day: 'numeric', weekday: 'long'};
+//     const currentDate = date.toLocaleDateString(lang, options);
+//     dateContainer.textContent = currentDate.charAt(0).toUpperCase() + currentDate.slice(1);
+//   }
 
 
 
